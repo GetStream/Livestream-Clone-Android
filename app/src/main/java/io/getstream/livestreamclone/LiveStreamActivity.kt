2949b0bce4be404dbe7a3050756c220ba14f8c96
Samuel Urbanowicz <samiuelson@gmail.com>
2020-05-05 13:01:55 +0200

    Animate chat on new message

--- app/src/main/java/io/getstream/livestreamclone/LiveStreamActivity.kt
+++ app/src/main/java/io/getstream/livestreamclone/LiveStreamActivity.kt
@@ -1,9 +1,11 @@
 package io.getstream.livestreamclone
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearSmoothScroller
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
@@ -12,6 +14,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
     private val adapter = MessagesListAdapter()
+
+    private val messageListSmoothScroller by lazy {
+        object : LinearSmoothScroller(this) {
+            val MILLISECONDS_PER_INCH = 400f
+
+            override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics?): Float {
+                return MILLISECONDS_PER_INCH / displayMetrics!!.densityDpi
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +36,7 @@ class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
             when (it) {
                 is State.Messages -> updateMessagesList(it.messages)
                 is State.NewMessage -> updateMessagesList(adapter.currentList + it.message)
-                is State.Error -> showToast("Message loading error: ${it.message}")
+                is State.Error -> showToast(it.message)
             }
         })
 
@@ -39,15 +51,17 @@ class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
     private fun updateMessagesList(messages: List<Message>) {
         adapter.submitList(messages)
         adapter.notifyDataSetChanged()
-        messagesList.smoothScrollToPosition(adapter.itemCount)
+        val scrollTarget = adapter.itemCount
+        messageListSmoothScroller.targetPosition = scrollTarget
+        messagesList.layoutManager?.startSmoothScroll(messageListSmoothScroller)
     }
 
     private fun loadMockVideoStream() {
         val playerListener = object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
-                val videoId = "DjTZ3UarzfU"
+                val youtubeVideoId = "XYqrrpvTtU8"
                 youTubePlayer.run {
-                    loadVideo(videoId, 0f)
+                    loadVideo(youtubeVideoId, 0f)
                 }
             }
         }
