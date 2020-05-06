51c720f1f509e5b535c08c41e9f561fac1c93d79
Samuel Urbanowicz <samiuelson@gmail.com>
2020-05-06 14:02:02 +0200

    Add tutorial step-3

--- app/src/main/java/io/getstream/livestreamclone/LiveStreamActivity.kt
+++ app/src/main/java/io/getstream/livestreamclone/LiveStreamActivity.kt
@@ -1,11 +1,9 @@
 package io.getstream.livestreamclone
 
 import android.os.Bundle
-import android.util.DisplayMetrics
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearSmoothScroller
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
@@ -14,16 +12,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
     private val adapter = MessagesListAdapter()
-
-    private val messageListSmoothScroller by lazy {
-        object : LinearSmoothScroller(this) {
-            val MILLISECONDS_PER_INCH = 400f
-
-            override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics?): Float {
-                return MILLISECONDS_PER_INCH / displayMetrics!!.densityDpi
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,8 +40,7 @@ class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
         adapter.submitList(messages)
         adapter.notifyDataSetChanged()
         val scrollTarget = adapter.itemCount
-        messageListSmoothScroller.targetPosition = scrollTarget
-        messagesList.layoutManager?.startSmoothScroll(messageListSmoothScroller)
+        messagesList.scrollToPosition(scrollTarget)
     }
 
     private fun loadMockVideoStream() {
